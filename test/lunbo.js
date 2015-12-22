50726a28a1e1fcaa9d818e6eaea41fae8e8a4709
Sun Lifeng <1104506375@qq.com>
2015-12-22 18:40:47 +0800

    xx

---
+++ test/lunbo.js
@@ -0,0 +1,55 @@
+﻿// 获取class
+function byClass(father, cName) {
+    var arr = [];
+    var c = father.getElementsByTagName("*");
+    for (var i = 0; i < c.length; i++) {
+        if (c[i].className == cName) {
+            arr.push(c[i]);
+        }
+    }
+    return arr;
+}
+
+function byCss(obj, attr) {
+    return obj.currentStyle ? obj.currentStyle[attr] : getComputedStyle(obj, null)[attr];
+}
+
+// 选项卡
+function Tabs(father) {
+    this.father = father;
+    this.slide = byClass(this.father, "nav")[0].children;
+    this.con = byClass(this.father, "content")[0].children;
+    this.iNow = 0;
+    this.init();
+}
+Tabs.prototype.init = function () {
+    var that = this;
+    for (var i = 0; i < this.slide.length; i++) {
+        this.slide[i].index = i;
+        this.slide[i].onclick = function () {
+            that.change(this);
+        }
+    }
+}
+Tabs.prototype.change = function (obj) {
+    for (var j = 0; i < this.slide.length; j++) {
+        this.slide[j].className = "";
+        this.con[j].style.display = "none";
+    }
+    if (obj) {
+        this.iNow = obj.index;
+    }
+    this.slide[this.iNow].className = "active";
+    this.con[this.iNow].style.display = "block";
+}
+Tabs.prototype.autoPlay = function () {
+    var This = this;
+    setInterval(function () {
+        if (This.iNow == This.slide.length - 1) {
+            This.iNow = 0;
+        } else {
+            This.iNow++;
+        }
+        This.change();
+    },2000)
+}
---
+++ test/obj-js.js
@@ -0,0 +1,68 @@
+﻿/* js 实现对象继承工具类包含实现方法_super,初始化函数为init可以传递构造方法参数
+ * date:2015-12-22
+ * name:Ufree
+ */
+
+(function (global) {
+    "use strict";
+    var Class = function () { };
+    // 实现super空实现
+    Class.prototype._super = function () {
+    };
+    Class.prototype.init = function () {
+    };
+    // 继承函数实现方法
+    function _extend(p) {
+        // 不执行初始化方法
+        var pt = new this(false);
+        if (typeof p == 'object') {
+            for (var name in p) {
+                // 属性不能包含_super保留字段
+                if (/\b_super\b/.test(name)) {
+                    if (console && typeof console.error == "function") {
+                        console.error("_super是保留字段不支持!");
+                    }
+                    continue;
+                }
+                if (/\binit\b/.test(name) && typeof p[name] != "function") {
+                    if (console && typeof console.error == "function") {
+                        console.error("init必须是函数类型!");
+                    }
+                    continue;
+                }
+                // 针对函数类型,实现父类super
+                if (typeof pt[name] == "function" && typeof p[name] == "function") {
+                    var tmpPt = pt[name];
+                    pt[name] = function () {
+                        var superTmp = this._super;
+                        this._super = tmpPt;
+                        p[name].apply(this, arguments);
+                        this._super = superTmp;
+                    }
+                } else {
+                    pt[name] = p[name];
+                }
+            }
+        }
+
+        var C = function () {
+            // 参数false不执行初始化方法
+            if (arguments.length > 0 && arguments[0] === false) {
+                return;
+            }
+            if (this.init && typeof this.init == "function") {
+                this.init.apply(this, arguments);
+            }
+        }
+        // 使用C作为构造函数，初始化执行init
+        pt.constructor = C;
+        C.prototype = pt;
+        C.extent = _extend;
+        C.create = function (p) {
+            return new C(p);
+        }
+        return C;
+    }
+    Class.extent = _extend;
+    global.Class = Class;
+})(window);
