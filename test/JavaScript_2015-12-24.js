570ee6df68a05fba53c503b58c7ec343bc3ceeb6
Sun Lifeng <1104506375@qq.com>
2015-12-24 18:52:30 +0800

    xx

---
+++ test/JavaScript_2015-12-24.js
@@ -0,0 +1,157 @@
+﻿// 参考地址:https://github.com/maxzhang/oop/blob/master/README.md
+// JavaScript面向对象编程，类声明、继承......
+// 声明类
+var MyCls = Klass.define({
+    construtor: function (name) { // 构造函数
+        this.name = name;
+    },
+    method1: function () {
+        console.log('hello ' + this.name);
+    },
+    method2: function (x, y) {
+        console.log(x + y);
+    },
+    method3: function (x, y) {
+        console.log(x * y);
+    }
+});
+
+new MyCls('max').method1(); // print 'hello world'
+
+// 继承类
+var SubCls1 = Klass.define(MyCls, {
+    method1: function () { // 重写父类方法
+        console.log('hi ' + this.name);
+    },
+    method4: function () { // 新方法
+
+    }
+});
+new SubCls1('max').method1(); // print 'hi world'
+
+// 调用父类方法
+var SubCls2 = Klass.define(MyCls, {
+    method1: function () {
+        this.callParent(); // 调用父类方法
+        console.log('hi ' + this.name);
+    },
+    method2: function () {
+        this.callParent([++x, y]); // 传入参数集合
+    },
+    method2: function () {
+        this.callParent(arguments); // 也可以直接传入arguments
+    }
+});
+
+// 声明静态属性和方法
+var SubCls3 = Klass.define(MyCls, {
+    statics: { // 静态属性和方法
+        prop1: 'static property',
+        fn1: function () {
+            console.log('this is a ' + SubCls3.prop1);
+        }
+    }
+});
+
+// 参考地址:https://github.com/yangmingkun187/JavaScript_oo
+// 如何定义类：
+// 1.构造函数法
+function Dog() {
+    this.name = "大毛";
+}
+Dog.prototype.makeSound = function () {
+    alert("旺旺旺");
+}
+var dog1 = new Dog();
+alert(dog1.name); // 大毛
+
+// 2.Object.create()法
+// "类"就是一个对象，不是函数
+var Dog = {
+    name: "大毛",
+    makeSound: function () {
+        alert("汪汪汪");
+    }
+}
+// 用Object.create()生成实例，不需要用到new
+var dog1 = Object.create(Dog);
+alert(dog1.name); // 大毛
+alert(dog1.makeSound()); // 汪汪汪
+
+// 这种方法比"构造函数"简单，但不能实现私有属性和私有方法，实例之间也不能模拟数据，对
+// 类的模拟不够全面
+
+// 3.极简主义法
+// 3.1 封装
+var Dog = {
+    createNew: function () { // 构造函数
+        var dog = {};
+        dog.name = "大毛";
+        dog.makeSound = function () {
+            alert("汪汪汪");
+        }
+        return dog;
+    }
+};
+// 调用
+var dog1 = Dog.createNew();
+dog1.makeSound(); // 汪汪汪
+
+// 3.2 继承
+var Animal = {
+    createNew: function () {
+        var animal = {};
+        animal.sleep = function () {
+            alert("睡懒觉!");
+        }
+        return animal;
+    }
+};
+
+// 在Cat的createNew()方法中，调用Animal的createNew()方法
+var Cat = {
+    createNew: function () {
+        var cat = Animal.createNew();
+        cat.name = "大毛";
+        cat.makeSound = function () {
+            alert("喵喵喵");
+        }
+        return cat;
+    }
+}
+
+// 这样得到的Cat实例，就会同时继承Cat和Animal类
+var cat1 = Cat.createNew();
+cat1.sleep(); // 睡懒觉
+
+// 3.3 私有属性和方法
+var Cat = {
+    createNew: function () {
+        var cat = {};
+        var sound = "喵喵喵";
+        cat.makeSound = function () {
+            alert(sound);
+        }
+        return cat;
+    }
+}
+// 上例的内部变量sound,外部无法读取，只有通过Cat的公有方法来读取
+var cat1 = Cat.createNew();
+alert(cat1.sound); // undefined
+
+// 有时候，我们需要所有实例对象，能够读写同一项内部数据。这个时候
+// 只要把这个内部数据，封装在类对象的里面，createNew()方法的外面即可
+var Cat = {
+    sound: "喵喵",
+    createNew: function () {
+        var cat = {};
+        cat.makeSound = function () { alert(Cat.sound); };
+        cat.changeSound = function (x) { Cat.sound = x; };
+        return cat;
+    }
+};
+
+// 然后生成两个实例对象：
+var cat1 = Cat.createNew();
+var cat2 = Cat.createNew();
+cat1.makeSound(); // 喵喵喵
---
+++ test/JavaScript核心概念.js
@@ -0,0 +1,58 @@
+﻿// 1、对象
+var foo = {
+    x: 10,
+    y: 20
+};
+// 两个明显的自身属性和一个隐含的prototype属性
+
+// 原型链
+// 原型对象也是简单的对象并且可以拥有它们自己的原型。
+// 如果一个原型对象的原型是一个非null的引用，那么一次类推，这就叫原型链。
+// 原型链是一个用来实现继承和共享属性的有限对象链
+
+var a = {
+    x: 10,
+    calculate: function (z) {
+        return this.x + this.y + z;
+    }
+}
+
+var b = {
+    y: 20,
+    _proto_: a
+}
+
+var c = {
+    y: 30,
+    _proto_: a
+}
+// call the inherited method,原型链继承
+b.calculate(30); // 60
+c.calculate(40); // 80  
+
+// 使用Object.create()函数
+var b = Object.create(a, { y: { value: 20 } });
+var c = Object.create(a, { y: { value: 30 } });
+
+// 构造函数
+// a constructor function
+function Foo(y) {
+    // which may create objects
+    // by specified pattern: thery have after
+    // creation own "y" property
+    this.y = y;
+}
+Foo.prototype.x = 10;
+Foo.prototype.calculate = function (z) {
+    return this.x + this.y + z;
+}
+
+var b = new Foo(20);
+var c = new Foo(30);
+
+// call the inherited method
+b.calculate(30); // 60
+c.calculate(40); // 80
+
+// 执行上下为堆栈
+
